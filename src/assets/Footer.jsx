18f5145f9482424cf9afe07a90fa4b14b2879501
yashjangir04 <jangiryash04@gmail.com>
2025-07-19 08:48:47 +0530

    Some changes in Footer

--- src/assets/Footer.jsx
+++ src/assets/Footer.jsx
@@ -10,10 +10,10 @@ const Footer = () => {
   return (
     <div className='bg-[#000000] w-full min-h-[10vh] py-4'>
       <div className="socials w-full h-full flex flex-row gap-12 justify-center items-center">
-        <a href='https://www.codechef.com/users/yashjangir04' target="_blank" className='text-[#3366CC] text-4xl hover:scale-110 duration-300'><SiCodechef /></a>
-        <a href='https://codeforces.com/profile/yashjangir04' target="_blank" className='text-[#008000] text-4xl hover:scale-110 duration-300'><SiCodeforces /></a>
-        <a href='https://www.linkedin.com/in/yashjangir04/' target="_blank" className='text-[#0077B5] hover:scale-110 duration-300 text-4xl'><FaLinkedin /></a>
-        <a href='mailto:connect.to.yj@gmail.com' target="_blank" className='text-[#D14836] hover:scale-110 duration-300 text-4xl'><BiLogoGmail /></a>
+        <a href='https://www.codechef.com/users/yashjangir04' target="_blank" className='text-[#3366CC] text-4xl hover:scale-110 hover:rotate-y-45 transition-rotate duration-500'><SiCodechef /></a>
+        <a href='https://codeforces.com/profile/yashjangir04' target="_blank" className='text-[#008000] text-4xl hover:scale-110 hover:rotate-y-45 transition-rotate duration-500'><SiCodeforces /></a>
+        <a href='https://www.linkedin.com/in/yashjangir04/' target="_blank" className='text-[#0077B5] text-4xl hover:scale-110 hover:rotate-y-45  duration-500'><FaLinkedin /></a>
+        <a href='mailto:connect.to.yj@gmail.com' target="_blank" className='text-[#D14836] text-4xl hover:scale-110 hover:rotate-y-45  duration-500'><BiLogoGmail /></a>
       </div>
     </div>
   )
